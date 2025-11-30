9af30a80c5d3aca91f45d1ca1ec7738ec15c36e1
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2025-11-30 13:40:33 +0900

    ✨ 환영 메시지 텍스트 위젯 분리
    - `WelcomeMessagePage`에 있던 환영 메시지 `Text` 위젯들을 `ScrollAnimatedWelcomeMessageText` 컴포넌트로 분리하여 리팩토링했습니다.

---
+++ lib/feature/welcome/components/scroll_animated_welcome_message_text.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/widgets.dart';
+
+import '../../../shared/ui/theme/custom_color.dart';
+
+class ScrollAnimatedWelcomeMessageText extends StatelessWidget {
+  const ScrollAnimatedWelcomeMessageText({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [
+      const Text(
+        "1월에 진행된 Reflection 이후\n시간이 정말 빠르게 흘렀네요",
+        textAlign: TextAlign.center,
+        style: TextStyle(
+          color: CustomColor.white,
+          fontSize: 40,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      const Text(
+        "정신없이 달리다보니 벌써 이번 년도의\n끝을 향해 달려가고 있네요.",
+        textAlign: TextAlign.center,
+        style: TextStyle(
+          color: CustomColor.white,
+          fontSize: 40,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      const Text(
+        "작년, 올해 초에 계획했던 이루고 계신가요?",
+        textAlign: TextAlign.center,
+        style: TextStyle(
+          color: CustomColor.white,
+          fontSize: 40,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      const Text(
+        "뭐 아무튼 다시 새로운 한 해를 맞이하며\nReflection 2026 동문회에 여러분을 초대합니다",
+        textAlign: TextAlign.center,
+        style: TextStyle(
+          color: CustomColor.white,
+          fontSize: 40,
+          fontWeight: FontWeight.bold,
+        ),
+      )
+    ],);
+  }
+}
--- lib/feature/welcome/welcome_message_page.dart
+++ lib/feature/welcome/welcome_message_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:reflection2026/shared/ui/theme/custom_color.dart';
 
 import '../../gen/assets.gen.dart';
+import 'components/scroll_animated_welcome_message_text.dart';
 
 class WelcomeMessagePage extends StatelessWidget {
   const WelcomeMessagePage({Key? key}) : super(key: key);
@@ -14,48 +15,13 @@ class WelcomeMessagePage extends StatelessWidget {
         Padding(
           padding: const EdgeInsets.only(bottom: 200),
           child: Column(
+            spacing: 56,
             children: [
               Assets.images.ufoAndIcon.image(
                 width: 724,
                 height: 733,
               ),
-              const SizedBox(height: 56),
-              const Text(
-                "1월에 진행된 Reflection 이후\n시간이 정말 빠르게 흘렀네요",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  color: CustomColor.white,
-                  fontSize: 40,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              const Text(
-                "정신없이 달리다보니 벌써 이번 년도의\n끝을 향해 달려가고 있네요.",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  color: CustomColor.white,
-                  fontSize: 40,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              const Text(
-                "작년, 올해 초에 계획했던 이루고 계신가요?",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  color: CustomColor.white,
-                  fontSize: 40,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              const Text(
-                "뭐 아무튼 다시 새로운 한 해를 맞이하며\nReflection 2026 동문회에 여러분을 초대합니다",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  color: CustomColor.white,
-                  fontSize: 40,
-                  fontWeight: FontWeight.bold,
-                ),
-              )
+              const ScrollAnimatedWelcomeMessageText(),
             ],
           ),
         ),
