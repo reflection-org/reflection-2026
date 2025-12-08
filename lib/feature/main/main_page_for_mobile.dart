5dd34411d6296108c5676ff7ffad32892aae792a
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2025-12-08 20:46:59 +0900

    ✨ 모바일 화면 개발중 안내 페이지 추가
    - `main_page_for_mobile.dart`에 "모바일은 아직 개발중이에요" 안내 UI를 구현했습니다.
    - 로고 및 안내 이미지(`waiting.png`) 애셋을 추가했습니다.

--- lib/feature/main/main_page_for_mobile.dart
+++ lib/feature/main/main_page_for_mobile.dart
@@ -1,12 +1,64 @@
 import 'package:flutter/material.dart';
+import 'package:reflection2026/shared/ui/theme/custom_color.dart';
+
+import '../../gen/assets.gen.dart';
 
 class MainPageForMobile extends StatelessWidget {
   const MainPageForMobile({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder(
-
+    return Scaffold(
+      backgroundColor: CustomColor.black,
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 20),
+              child: Assets.icons.reflection.svg(
+                colorFilter: const ColorFilter.mode(
+                  CustomColor.white,
+                  BlendMode.srcIn,
+                ),
+                width: 149,
+                height: 20,
+              ),
+            ),
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Assets.images.waiting.image(width: 215, height: 104),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  const Text(
+                    "모바일은 아직 개발중이에요.",
+                    style: TextStyle(
+                      color: CustomColor.white,
+                      fontSize: 15,
+                      fontWeight: FontWeight.w800,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 4,
+                  ),
+                  Text(
+                    "PC 버전으로 이용해주세요.",
+                    style: TextStyle(
+                      color: CustomColor.white.withValues(alpha: 0.5),
+                      fontSize: 12,
+                      fontWeight: FontWeight.w800,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
